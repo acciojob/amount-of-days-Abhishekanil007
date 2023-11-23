3a713420dd3bd71c7f35e4a0badc933833eb26ff
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-23 20:40:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function daysOfAYear(a){
+	if(a%4===0)
+			{
+				if(a%100===0)
+				{
+					if(a%400===0)
+					{
+						return 366;
+					}
+					else
+					{
+						return 365;
+					}
+				}
+				else
+				{
+					return 366;
+				}
+			}
+		    else
+			{
+				return 365;
+			}
+				
+}
+let yearToTake = 2023;
+let result = daysOfAYear(yearToTake);
+console.log(result);
+
